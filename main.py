65ce8239c797b081d4c2d2f503029eeca578caa9
Chr0nos <snicolet@student.42.fr>
2018-11-23 12:37:30 +0100

    wip

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, abort
 from relay import Relay
 app = Flask(__name__)
 
@@ -6,3 +6,21 @@ app = Flask(__name__)
 def main():
     return "Hello world"
 
+@app("/set/<port>/<state>", methods=['POST'])
+def set_relay_state(port, state):
+    try:
+        port = int(port)
+        state = bool(state)
+        r = Relay()
+        r.setEnabled(port, state)
+        return "OK"
+    except ValueError:
+        abort(400)
+    except IndexError:
+        abort(406)
+
+@app("/get/<port>", methods=['GET'])
+def get_relay_state(port):
+    try:
+        port = int(port)
+        return "TODO"
--- relay.py
+++ relay.py
@@ -8,13 +8,17 @@ class Relay:
 	def __init__(self, fullinit=False):
 		if fullinit:
 			GPIO.setmode(GPIO.BOARD)
+        else:
+            GPIO.setwarnings(False)
 		self.states = [False, False, False, False]
 		self.pins = [12, 11, 10, 8]
+        self.fullinit = fullinit
 		for p in self.pins:
 			GPIO.setup(p, GPIO.OUT)
 
 	def __del__(self):
-		GPIO.cleanup()
+        if self.fullinit:
+            GPIO.cleanup()
 
 	def setEnabled(self, index, state):
 		self.states[index] = state
@@ -30,3 +34,7 @@ class Relay:
 			self.setEnabled(i, True)
 			time.sleep(1)
 		self.setAll(False)
+
+    def update(self):
+        for i in range(0, len(self.pins)):
+            pass
