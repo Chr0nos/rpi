ec7788946923bfca8662f74ac00a7a2322b9a483
Chr0nos <snicolet@student.42.fr>
2018-11-23 14:45:14 +0100

    api is now working fine.

--- main.py
+++ main.py
@@ -2,25 +2,34 @@ from flask import Flask, abort
 from relay import Relay
 app = Flask(__name__)
 
+DB = "states.json"
+
 @app.route("/", methods=['GET'])
 def main():
-    return "Hello world"
+	return "Hello world"
 
-@app("/set/<port>/<state>", methods=['POST'])
+@app.route("/set/<port>/<state>", methods=['POST'])
 def set_relay_state(port, state):
-    try:
-        port = int(port)
-        state = bool(state)
-        r = Relay()
-        r.setEnabled(port, state)
-        return "OK"
-    except ValueError:
-        abort(400)
-    except IndexError:
-        abort(406)
+	try:
+		port = int(port)
+		state = bool(state)
+		Relay(database=DB).setEnabled(port, state)
+		return "OK\n"
+	except (ValueError, TypeError) as e:
+		abort(400)
+	except IndexError:
+		abort(406)
 
-@app("/get/<port>", methods=['GET'])
+@app.route("/get/<port>", methods=['GET'])
 def get_relay_state(port):
-    try:
-        port = int(port)
-        return "TODO"
+	try:
+		port = int(port)
+		Relay(database=DB).update()
+		return "TODO"
+	except TypeError:
+		abort(400)
+
+@app.route("/reset", methods=['POST'])
+def reset():
+	Relay(True).setAll(False)
+	return "OK\n"
--- relay.py
+++ relay.py
@@ -3,22 +3,24 @@ import RPi.GPIO as GPIO
 import schedule
 import time
 import datetime
+import json
 
 class Relay:
-	def __init__(self, fullinit=False):
-		if fullinit:
-			GPIO.setmode(GPIO.BOARD)
-        else:
-            GPIO.setwarnings(False)
+	def __init__(self, fullinit=False, database=None):
 		self.states = [False, False, False, False]
 		self.pins = [12, 11, 10, 8]
-        self.fullinit = fullinit
-		for p in self.pins:
-			GPIO.setup(p, GPIO.OUT)
+		self.fullinit = fullinit
+		self.database = database
+		GPIO.setwarnings(False)
+		GPIO.setmode(GPIO.BOARD)
+		GPIO.setup(self.pins, GPIO.OUT, initial=GPIO.HIGH)
+		if self.load():
+			self.update()
 
 	def __del__(self):
-        if self.fullinit:
-            GPIO.cleanup()
+		self.save()
+		if self.fullinit:
+			GPIO.cleanup()
 
 	def setEnabled(self, index, state):
 		self.states[index] = state
@@ -35,6 +37,28 @@ class Relay:
 			time.sleep(1)
 		self.setAll(False)
 
-    def update(self):
-        for i in range(0, len(self.pins)):
-            pass
+	def update(self):
+		for i in range(0, len(self.pins)):
+			pin = self.pins[i]
+			state = self.states[i]
+			self.setEnabled(i, state)
+
+	def save(self):
+		if not self.database:
+			return
+		with open(self.database, 'w') as fd:
+			json.dump({'states': self.states}, fd)
+			fd.close()
+		# print("saved")
+
+	def load(self):
+		if not self.database:
+			return False
+		try:
+			with open(self.database, 'r') as fd:
+				self.states = json.load(fd)['states']
+				print(self.states)
+			# print("loaded")
+			return True
+		except FileNotFoundError:
+			return False
